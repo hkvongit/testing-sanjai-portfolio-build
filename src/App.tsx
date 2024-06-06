2b96ff731401aa055f8bfa9a6fd0e9ab10e3cf17
hari anitha <hv8447@gmail.com>
2024-06-06 15:29:17 +0530

    Testing build

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,47 @@
-import { NavBar } from './components/navbar'
-import { Content } from './components/contents';
-import { Skills } from './components/skills'
-import { About } from './components/about'
-import { Projects } from './components/projects'
-import { ContactForm } from './components/contactForm';
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NavBar } from "./components/navbar";
+import { Content } from "./components/contents";
+import { Skills } from "./components/skills";
+import { About } from "./components/about";
+import { Projects } from "./components/projects";
+import { ContactForm } from "./components/contactForm";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-     
-          <Routes>
-            <Route path="/" element={<>
-              <NavBar />
-              <Content />
-              <About />
-              <Skills />
-              <Projects />
-              <ContactForm/>
-            </>} />
-            <Route path="/about" element={<>
-              <NavBar />
-              <About />
-            </>} />
-            <Route path="/skills" element={<>
-              <NavBar />
-              <Skills />
-            </>} />
-            <Route path="/projects" element={<>
-              <NavBar />
-              <Projects />
-            </>} />
-            <Route path="/footer" element={<>
-              <NavBar />
-              <ContactForm/>
-            </>} />
-          </Routes>
-  
-    </BrowserRouter>
-  )
+    <div>
+      <Projects />
+    </div>
+    // <BrowserRouter>
+    //       <Routes>
+    //         <Route path="/" element={<>
+    //           <NavBar />
+    //           <Content />
+    //           <About />
+    //           <Skills />
+    //           <Projects />
+    //           <ContactForm/>
+    //         </>} />
+    //         <Route path="/about" element={<>
+    //           <NavBar />
+    //           <About />
+    //         </>} />
+    //         <Route path="/skills" element={<>
+    //           <NavBar />
+    //           <Skills />
+    //         </>} />
+    //         <Route path="/projects" element={<>
+    //           <NavBar />
+    //           <Projects />
+    //         </>} />
+    //         <Route path="/footer" element={<>
+    //           <NavBar />
+    //           <ContactForm/>
+    //         </>} />
+    //       </Routes>
+
+    // </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

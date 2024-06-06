946270179992a351387a914087373811c33eafda
hari anitha <hv8447@gmail.com>
2024-06-06 15:47:25 +0530

    Changed to hash router from browser router

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,63 @@ import { About } from "./components/about";
 import { Projects } from "./components/projects";
 import { ContactForm } from "./components/contactForm";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Projects />
-    </div>
-    // <BrowserRouter>
-    //       <Routes>
-    //         <Route path="/" element={<>
-    //           <NavBar />
-    //           <Content />
-    //           <About />
-    //           <Skills />
-    //           <Projects />
-    //           <ContactForm/>
-    //         </>} />
-    //         <Route path="/about" element={<>
-    //           <NavBar />
-    //           <About />
-    //         </>} />
-    //         <Route path="/skills" element={<>
-    //           <NavBar />
-    //           <Skills />
-    //         </>} />
-    //         <Route path="/projects" element={<>
-    //           <NavBar />
-    //           <Projects />
-    //         </>} />
-    //         <Route path="/footer" element={<>
-    //           <NavBar />
-    //           <ContactForm/>
-    //         </>} />
-    //       </Routes>
-
-    // </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <NavBar />
+              <Content />
+              <About />
+              <Skills />
+              <Projects />
+              <ContactForm />
+            </>
+          }
+        />
+        <Route
+          path='/about'
+          element={
+            <>
+              <NavBar />
+              <About />
+            </>
+          }
+        />
+        <Route
+          path='/skills'
+          element={
+            <>
+              <NavBar />
+              <Skills />
+            </>
+          }
+        />
+        <Route
+          path='/projects'
+          element={
+            <>
+              <NavBar />
+              <Projects />
+            </>
+          }
+        />
+        <Route
+          path='/footer'
+          element={
+            <>
+              <NavBar />
+              <ContactForm />
+            </>
+          }
+        />
+      </Routes>
+    </HashRouter>
   );
 }
 
